e999c44c74d220274203bfc58944d0bf79ec38f9
Alecio <aleciosilveira97@gmail.com>
2024-12-05 00:27:42 -0300

    Configurando API json Server para usar como teste

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common'; // Import necessário para diretivas como *ngFor
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Inclua o CommonModule aqui
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrls: ['./list.component.scss'] // Corrigido o nome da propriedade (styleUrls)
 })
 export class ListComponent {
+  products: any[] = [];
 
+  httpClient = inject(HttpClient);
+
+  ngOnInit() {
+    this.httpClient.get<any>('/api/products').subscribe((products) => {
+      this.products = products;
+    });
+  }
 }
+
