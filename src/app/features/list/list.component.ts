9f9cbd542768ab67abfadcb28bc8c8c33545038c
Alecio <aleciosilveira97@gmail.com>
2024-12-06 00:01:44 -0300

    Utilizando material para colocar botoes

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common'; // Import necessário para diretivas como *ngFor
 import { ProductsService } from '../../shared/service/products.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule], // Inclua o CommonModule aqui
+  imports: [CommonModule, MatCardModule, MatButtonModule], // Inclua o CommonModule aqui
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'] // Corrigido o nome da propriedade (styleUrls)
 })
